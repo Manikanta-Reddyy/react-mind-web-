04ef06253d08451ca16cd922a68334db9ac0cc38
Manikanta-Reddyy <94749162+Manikanta-Reddyy@users.noreply.github.com>
2025-08-06 16:36:23 +0530

    Update useStore.ts

--- src/context/useStore.ts
+++ src/context/useStore.ts
@@ -1,19 +1,19 @@
 import { create } from "zustand";
 
 type TimeSelection = {
-  mode: "single" | "range";
-  selected: number | [number, number];
-  setMode: (mode: "single" | "range") => void;
-  setSelected: (value: number | [number, number]) => void;
+  mode: "single" | "range";
+  selected: number | [number, number];
+  setMode: (mode: "single" | "range") => void;
+  setSelected: (value: number | [number, number]) => void;
 };
 
 export const useTimeStore = create<TimeSelection>((set) => ({
-  mode: "single",
-  selected: 360,
-  setMode: (mode) =>
-    set((state) => ({
-      mode,
-      selected: mode === "range" ? [340, 380] : 360, // ✅ fix here
-    })),
-  setSelected: (value) => set({ selected: value }),
+  mode: "single",
+  selected: 360,
+  setMode: (mode) =>
+    set({
+      mode,
+      selected: mode === "range" ? [340, 380] : 360,
+    }),
+  setSelected: (value) => set({ selected: value }),
 }));
